433fd5c612239465808c4ede1c0de481b69134bb
Pon157 <Vitechek.208@gmail.com>
2025-10-29 18:18:07 +0300

    Update message.js

--- backend/models/message.js
+++ backend/models/message.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+  from: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  to: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+  type: { type: String, enum: ['text', 'image', 'sticker', 'file'], default: 'text' },
+  fileName: { type: String }, // для файлов
+  sentAt: { type: Date, default: Date.now },
+  read: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
 
