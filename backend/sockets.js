405fffb4fd083b0297aa4d321a59a1c58e23d885
Pon157 <Vitechek.208@gmail.com>
2025-10-29 18:08:39 +0300

    Update sockets.js

--- backend/sockets.js
+++ backend/sockets.js
@@ -1,1 +1,28 @@
+module.exports = (io) => {
+  const connectedUsers = {};
+
+  io.on('connection', (socket) => {
+    socket.on('authorize', (token) => {
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return socket.disconnect();
+        connectedUsers[socket.id] = decoded.id;
+        socket.join(decoded.id.toString());
+      });
+    });
+
+    socket.on('sendMessage', async (data) => {
+      const { to, content, type } = data;
+      const from = connectedUsers[socket.id];
+
+      const message = new Message({ from, to, content, type, sentAt: new Date() });
+      await message.save();
+
+      io.to(to.toString()).emit('newMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+      delete connectedUsers[socket.id];
+    });
+  });
+};
 
