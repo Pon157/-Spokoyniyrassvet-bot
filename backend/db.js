3d520faf216cf949860823a444241acd0ba58eff
Pon157 <Vitechek.208@gmail.com>
2025-10-29 22:24:22 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -6,9 +6,9 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error('Database connection error:', error);
+        console.error('❌ Database connection error:', error);
         process.exit(1);
     }
 };
