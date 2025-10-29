94eb047eebc9f2e9476a3be00e3cad511fdd4005
Pon157 <Vitechek.208@gmail.com>
2025-10-29 21:37:16 +0300

    Update log.js

--- backend/models/log.js
+++ backend/models/log.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 
 const logSchema = new mongoose.Schema({
-  action: {
-    type: String,
-    required: true
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  targetId: mongoose.Schema.Types.ObjectId,
-  details: mongoose.Schema.Types.Mixed,
-  timestamp: {
-    type: Date,
-    default: Date.now
-  }
+    action: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    targetId: mongoose.Schema.Types.ObjectId,
+    details: mongoose.Schema.Types.Mixed,
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model('Log', logSchema);
