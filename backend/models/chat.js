a2e39febeb326948bbb4b48d5d1be9d335459410
Pon157 <Vitechek.208@gmail.com>
2025-10-29 18:19:04 +0300

    Update chat.js

--- backend/models/chat.js
+++ backend/models/chat.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const ChatSchema = new mongoose.Schema({
+  users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
+  createdAt: { type: Date, default: Date.now }
+});
+
+// Индексы для оптимизации запросов
+ChatSchema.index({ users: 1 });
+
+module.exports = mongoose.model('Chat', ChatSchema);
 
