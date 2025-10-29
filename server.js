7c4df6876677178fe05ae452ad731ed879a53a1e
Pon157 <Vitechek.208@gmail.com>
2025-10-29 22:01:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = socketIo(server, {
     }
 });
 
-// Middleware  
+// Middleware  ww
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
