851c76a368fd6abb5ea470a4280106bc4fd5f3d2
Pon157 <Vitechek.208@gmail.com>
2025-10-31 17:15:30 +0300

    Update coowner.js

--- backend/controllers/coowner.js
+++ backend/controllers/coowner.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test', (req, res) => {
-    res.json({ message: 'Listener route works' });
+    res.json({ message: 'Coowner route works' });
 });
 
 module.exports = router;
